c7920439a7cbd84b1dd302dd0697238449c0eec0
Magicsmoke_mit_Lothar <69142791+Magicsmoke-mit-Lothar@users.noreply.github.com>
2024-02-16 14:08:15 +0100

    more animations for the index site

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function changeText(text, text_alt) {
     // Hide the original button text
       document.getElementById(text).style.display = "none";
       document.getElementById(text_alt).style.display = "block";
+      document.getElementById(text_alt).classList.add("redFlash");
 
   }
 
@@ -10,4 +11,5 @@ function changeText(text, text_alt) {
     document.getElementById(text).style.display = "block";
 
     document.getElementById(text_alt).style.display = "none";
+    document.getElementById(text_alt).classList.remove("redFlash");
   }
